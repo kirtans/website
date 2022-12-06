f6d545ab0c077b2367c86eddb98121b91399a76b
Kirtan Patel <kpatel@bolt.com>
2022-12-06 13:51:04 -0500

    Updates

---
+++ precache-manifest.04b41d97f651c620780c4f0958bb1be6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b5aa8c805924ac3326",
+    "url": "/static/css/main.8b0cd785.chunk.css"
+  },
+  {
+    "revision": "b6b5aa8c805924ac3326",
+    "url": "/static/js/main.b6b5aa8c.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "062185094b233777c4a1",
+    "url": "/static/js/2.06218509.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "ee4edf5d2e8b3336f289d93a097e50c5",
+    "url": "/static/media/OkineSans-Regular.ee4edf5d.otf"
+  },
+  {
+    "revision": "26d9974a1feec9905a4e0d5e5ddf8db6",
+    "url": "/static/media/instagram.26d9974a.png"
+  },
+  {
+    "revision": "147e05ea086d7172e12dbb29655f25ff",
+    "url": "/static/media/linkedin.147e05ea.png"
+  },
+  {
+    "revision": "3f7189662f19f8318fc75252deee723a",
+    "url": "/static/media/twitter.3f718966.png"
+  },
+  {
+    "revision": "282922c94b0e2453247045266df6a09c",
+    "url": "/static/media/profile_pic.282922c9.jpg"
+  },
+  {
+    "revision": "c24630532b92daea94473e8c32acc939",
+    "url": "/index.html"
+  }
+];
