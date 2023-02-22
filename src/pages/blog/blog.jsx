188944a78d049be117b87c756eab599c90ef3f79
Kirtan <Kirtansp1@gmail.com>
2023-02-21 22:41:01 -0500

    add helmet to blog

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -7,6 +7,11 @@ import Blog20230220 from "./blog20230220";
 const Blog = () => {
   return (
     <div className="blogContent">
+        <Helmet>
+            <title>Kirtan Patel</title>
+            <meta name="description" content="Hi! I am Kirtan Patel. There my thoughts on tech, education, and sharing positivity." />
+            <meta name="theme-color" content="#222222" />
+        </Helmet>
         <div>
             <Blog20230220/>
             <Blog20230207/>
