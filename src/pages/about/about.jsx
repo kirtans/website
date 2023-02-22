7264de32b1d2ea41d48a4104d7ccc4b2639172d9
Kirtan <Kirtansp1@gmail.com>
2023-02-21 23:03:34 -0500

    fix helmet and about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -7,14 +7,14 @@ const About = () => {
   return (
       <div className="body">
           <Helmet>
-              <title>About Me</title>
+              <title>About</title>
               <meta name="description" content="About Me" />
           </Helmet>
           <h2>About</h2>
           <div className="aboutContainer">
               <div>
                   <p>
-                      Hi! I am Kirtan Patel. I graduated from Georgia Tech in 2018 with a Bachelors in Computer Science. I have worked at Peloton and am currently working at Bolt. Technology and fitness are my main two passions and I strive to share topics that I find are important to make the world a little better through technology and education.
+                      Hi! I am Kirtan Patel. I graduated from Georgia Tech in 2018 with a Bachelors in Computer Science. I have been messing around with technology for over half my life. Health and Wellness, Education, and Technology are my main passions. I strive to find ways to share more about these topics in order to help make the world a little bit brighter.
                   </p>
                   <p>
                       I will also be adding some of my other passions and ventures to this site!
--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -8,11 +8,6 @@ import Blog20230220 from "./blog20230220";
 const Blog = () => {
   return (
     <div className="blogContent">
-        <Helmet>
-            <title>Kirtan Patel</title>
-            <meta name="description" content="Hi! I am Kirtan Patel. There my thoughts on tech, education, and sharing positivity." />
-            <meta name="theme-color" content="#222222" />
-        </Helmet>
         <div>
             <Blog20230220/>
             <Blog20230207/>
@@ -20,6 +15,11 @@ const Blog = () => {
         <div className="blogList">
             <BlogList/>
         </div>
+        <Helmet>
+            <title>Kirtan Patel</title>
+            <meta name="description" content="Hi! I am Kirtan Patel. There my thoughts on tech, education, and sharing positivity." />
+            <meta name="theme-color" content="#222222" />
+        </Helmet>
     </div>
   );
 }
